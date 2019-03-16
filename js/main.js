82b69406e8e5763a385b852fed8c1409ff536288
eng alaa_elzayat <alaaelzayat2018@gmail.com>
2019-03-16 23:10:14 +0200

    new

--- js/main.js
+++ js/main.js
@@ -26,7 +26,17 @@ $('.slider').bxSlider({
   pager:false
 });
 
-});
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementByclass("navbar").style.top = "0";
+  } else {
+    document.getElementByclass("navbar").style.top = "-50px";
+  }
+}
+
+});
 
 
