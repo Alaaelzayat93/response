692f614bda10b5a9e54d5aaabc4744466d7d2ec2
eng alaa_elzayat <alaaelzayat2018@gmail.com>
2019-03-25 12:49:26 +0200

    new

--- js/main.js
+++ js/main.js
@@ -49,19 +49,19 @@ navbar.removeClass('scrolled navbar-fixed-top');
 });
 });
 /*carousel*/
-$('.owl-carousel').owlCarousel({
-    
-   
+$('.owl-one').owlCarousel({
+    loop:true,
+    margin:10,
     nav:true,
     responsive:{
         0:{
-            items:1
-        },
-        600:{
             items:3
         },
+        600:{
+            items:2
+        },
         1000:{
-            items:4
+            items:1
         }
     }
 })
